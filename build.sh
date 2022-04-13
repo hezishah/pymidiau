90e566feba31e20191caae58e19a963db71f82fd
Hezi Shahmoon <hshahmoon@apple.com>
2022-04-13 14:55:06 +0300

    Add Python c interface
    Signed-off-by: Hezi Shahmoon <hshahmoon@apple.com>

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 llvm-gcc midi-au.c -framework AudioUnit -framework AudioToolbox -framework CoreMidi -framework CoreFoundation -o au
+llvm-gcc -shared pymidiau.c $(python3-config --include) -lpython -o pymidiau.so
---
+++ pymidiau.c
@@ -0,0 +1,33 @@
+#include <Python.h>
+
+
+static PyObject *method_midiau(PyObject *self, PyObject *args) {
+    char *arg1, *arg2;
+    int bytes_copied = -1;
+
+    /* Parse arguments */
+    if(!PyArg_ParseTuple(args, "ss", &arg1, &arg2)) {
+        return NULL;
+    }
+
+    printf("Hello %s %s\n", arg1,arg2);
+    return PyLong_FromLong(bytes_copied);
+}
+
+static PyMethodDef FputsMethods[] = {
+    {"midiau", method_midiau, METH_VARARGS, "Python interface for midiau C library function"},
+    {NULL, NULL, 0, NULL}
+};
+
+
+static struct PyModuleDef midiaumodule = {
+    PyModuleDef_HEAD_INIT,
+    "midiau",
+    "Python interface for the midiau C library function",
+    -1,
+    FputsMethods
+};
+
+PyMODINIT_FUNC PyInit_pymidiau(void) {
+    return PyModule_Create(&midiaumodule);
+}
