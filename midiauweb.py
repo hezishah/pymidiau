9ac6f80675d10ecdc78ed3c810229ad4120f4b18
Hezi Shahmoon <hshahmoon@apple.com>
2022-04-13 15:23:40 +0300

    Add a python code with web server
    Signed-off-by: Hezi Shahmoon <hshahmoon@apple.com>

---
+++ midiauweb.py
@@ -0,0 +1,13 @@
+import pymidiau
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello world'
+
+if __name__ == "__main__":
+    print("Hello")
+
+    app.run(debug=True, host='0.0.0.0')
